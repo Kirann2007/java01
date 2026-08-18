0eac197261ed00724fb2bd5c019abf12eef7f91d
KIRAN N <kirann1777@gmail.com>
2026-08-18 13:33:50 +0530

    UPdate JAVADSA code

---
+++ src/DAY2/patternpyramid.java
@@ -0,0 +1,18 @@
+package DAY2;
+
+public class patternpyramid {
+    public static void main(String[]args)
+    {
+        int rows=5;
+        for(int i=1;i<=rows;i++){
+            for(int s=1;s<=rows-i;s++){
+                System.out.print(" ");
+    }
+    for (int j=1;j<=(2*i-1);j++){
+        System.out.print("@&$");
+    }
+    System.out.println();
+
+        }
+    }
+}
