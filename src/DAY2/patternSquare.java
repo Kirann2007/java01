b6c478ac30a80f7fee0464cd89a32c8e8b56e237
KIRAN N <kirann1777@gmail.com>
2026-08-18 12:49:00 +0530

    UPdate JAVADSA code

--- src/DAY2/patternSquare.java
+++ src/DAY2/patternSquare.java
@@ -3,7 +3,7 @@ package DAY2;
 public class patternSquare {
     public static void main (String[]args)
     {
-        for(int i=1;i<=5;i++)
+        for(int i=1;i<=4;i++)
         {
             for(int j=1;j<=5;j++)
             {
--- src/DAY2/tn.java
+++ src/DAY2/tn.java
@@ -1,13 +1,14 @@
 package DAY2;
 
-public class PatternTriangle {
+public class tn {
     public static void main(String[]args)
     {
-        for(int i=1;i<=6;i++)
+        int num=1;
+        for (int i=1;i<=5;i++)
         {
             for(int j=1;j<=i;j++)
             {
-                System.out.print("^");
+                System.out.print(num++ + " ");
             }
             System.out.println();
         }
