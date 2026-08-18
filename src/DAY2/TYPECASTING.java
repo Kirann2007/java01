5c26bc8216ce1aa96ac94947fa5e7dddefd99d27
KIRAN N <kirann1777@gmail.com>
2026-08-18 09:42:57 +0530

    initial commit

---
+++ src/DAY2/TYPECASTING.java
@@ -0,0 +1,16 @@
+package DAY2;
+
+public class TYPECASTING {
+    public static void main(String[] args)
+    {
+        int number=100;
+
+        double value=number;
+        System.out.println(value);
+        double price=99.99;
+        System.out.println("the round off"+Math.round(price));
+
+        int converted=(int) price;
+        System.out.println(converted);
+    }
+}
