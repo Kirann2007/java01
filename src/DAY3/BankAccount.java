ecea1f8f6e6f91443dda09d48df415d56869d874
KIRAN N <kirann1777@gmail.com>
2026-08-19 09:51:36 +0530

    UPdate JAVADSA code

---
+++ src/DAY3/BankAccount.java
@@ -0,0 +1,8 @@
+package DAY3;
+
+public class BankAccount {
+    public static void main(String[]args){
+        BankAccount k=new BankAccount();
+        System.out.println("Bank account created"+k);
+    }
+}
---
+++ src/DAY3/STUDENT.java
@@ -0,0 +1,9 @@
+package DAY3;
+
+public class STUDENT {
+    public static void main(String[]args)
+    {
+        STUDENT s= new STUDENT();
+        System.out.println("Student object"+s);
+    }
+}
